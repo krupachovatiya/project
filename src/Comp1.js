fd36e1ec55f002192244e1dac91d1d64c936f6ab
krupachovatiya <chovatiyakrupa649@gmail.com>
2025-03-18 15:32:09 +0530

    add

--- src/Comp1.js
+++ src/Comp1.js
@@ -7,7 +7,7 @@ function Comp1(props) {
             <h2>Title: {props.title} </h2>
             <p>Description: {props.description} </p> 
 
-            <Comp2 title3 = 'The Power of Now'
+            <Comp2 title3 = 'The Power of Now is is '
             description3 = 'A spiritual exploration of mindfulness and living in the present moment.'
             title4 = 'The Psychology of Money'
             description4 = 'Insights into how emotions and behavior influence financial decisions.'/>       
